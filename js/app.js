9f009e7a60c8312fb7d012deb9659f92d5c33ef4
Sheldon Marsh <thesheldon@googlemail.com>
2016-11-26 13:41:15 +0100

    now with added STUFF

--- js/app.js
+++ js/app.js
@@ -6,11 +6,27 @@ var tashaSite = angular
   ])
   .controller('TashaController', function($scope, $route) {
         $scope.$route = $route;
+        if (window.twttr) {
+          window.twttr.widgets.load();
+        }
+  })
+  .directive("tmTweets", function() {
+  return { 
+      restrict: 'E',
+      template: '<a class="twitter-timeline"  href="https://twitter.com/tmwellbeing" data-widget-id="556111774169448449">Tweets by @tmwellbeing</a>',
+      link: function(scope, element, attrs) {
+        function tweetDisplay(){
+            !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");
+            console.log('run script');
+        };
+        tweetDisplay();
+      }
+    };
   })
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/test.html',
+        templateUrl: 'views/home.html',
         tab: 'mains',
         controller: 'TashaController'
       })
